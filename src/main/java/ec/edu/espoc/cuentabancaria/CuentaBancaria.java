b872c594893d2628709e2a35e7392ed5551815cd
OswaldoBifarini <jgbifarini@gmail.com>
2024-12-18 13:40:13 -0500

    Creacion de Repositorio

---
+++ src/main/java/ec/edu/espoc/cuentabancaria/CuentaBancaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoc.cuentabancaria;
+
+/**
+ *
+ * @author DELL
+ */
+public class CuentaBancaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
