abc4ac2ca1aafc88d6a51d6c44781dc2e36e5640
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-12-18 13:57:41 -0500

    hola

--- src/main/java/ec/edu/espoc/cuentabancaria/CuentaBancaria.java
+++ src/main/java/ec/edu/espoc/cuentabancaria/CuentaBancaria.java
@@ -11,6 +11,7 @@ package ec.edu.espoc.cuentabancaria;
 public class CuentaBancaria {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //hola
+        System.out.println("hola como estas");
     }
 }
