2dc018fd691aebdcc12bec60f107e6c30e008d20
anthonypulupa <anthonypulupa2@gmail.com>
2024-12-18 15:02:01 -0500

    Modificacion Clases

--- src/main/java/ec/edu/espoc/cuentabancaria/CuentaBancaria.java
+++ src/main/java/ec/edu/espoc/cuentabancaria/CuentaBancaria.java
@@ -1,16 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package ec.edu.espoc.cuentabancaria;
 
-/**
- *
- * @author DELL
- */
+
 public class CuentaBancaria {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public class Cuenta {
+
+    private float saldo;
+    private int numeroConsignaciones;
+    private int numeroRetiros;
+    private float tasaAnual;
+    private float comisionMensual;
+
+    public void Consignar(float cantidad) {
+     
+        saldo += cantidad;
+        numeroConsignaciones++;
     }
+
+    public void Retirar(float cantidad) {
+      
+        saldo -= cantidad;
+        numeroRetiros++;
+    }
+
+    public void ExtraerMensual() {
+       
+        saldo -= comisionMensual;
+    }
+
+    public void Imprimir() {
+   
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Número de consignaciones: " + numeroConsignaciones);
+        System.out.println("Número de retiros: " + numeroRetiros);
+        System.out.println("Tasa anual: " + tasaAnual);
+        System.out.println("Comisión mensual: " + comisionMensual);
+    }
+}
+
 }
--- src/main/java/ec/edu/espoc/cuentabancaria/clases/CuentaAhorros.java
+++ src/main/java/ec/edu/espoc/cuentabancaria/clases/CuentaAhorros.java
@@ -9,5 +9,41 @@ package ec.edu.espoc.cuentabancaria.clases;
  * @author anthony
  */
 public class CuentaAhorros {
+    public class CuentaAhorro extends Cuenta {
+    private boolean estaActiva;
+
+   
+    @Override
+    public void Consignar(float cantidad) {
+        // Lógica para consignar dinero en la cuenta de ahorro
+        if (estaActiva) {
+            super.Consignar(cantidad);
+        } else {
+            System.out.println("La cuenta de ahorro está inactiva.");
+        }
+    }
+
+ 
+    @Override
+    public void Retirar(float cantidad) {
+        // Lógica para retirar dinero de la cuenta de ahorro
+        if (estaActiva) {
+            super.Retirar(cantidad);
+        } else {
+            System.out.println("La cuenta de ahorro está inactiva.");
+        }
+    }
+
+   
+    @Override
+    public void ExtraerMensual() {
+        // Lógica para extraer la comisión mensual de la cuenta de ahorro
+        if (estaActiva) {
+            super.ExtraerMensual();
+        } else {
+            System.out.println("La cuenta de ahorro está inactiva.");
+        }
+    }
+}
     
 }
