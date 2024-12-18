c02fc25a88da7166cd6e9b966e10e527f5d61849
jenniffermuima. <jenniffermuima2001@gmail.com>
2024-12-18 14:32:36 -0500

    Creacion de clase Principal

--- src/main/java/ec/edu/espoc/cuentabancaria/CuentaBancaria.java
+++ src/main/java/ec/edu/espoc/cuentabancaria/CuentaBancaria.java
@@ -11,6 +11,37 @@ package ec.edu.espoc.cuentabancaria;
 public class CuentaBancaria {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    public float saldo;
+    private int numeroConsignaciones;
+    private int numeroRetiros;
+    private float tasaAnual;
+    private float comisionMensual;
+
+    public void Consignar(float cantidad) {
+     
+        saldo += cantidad;
+        numeroConsignaciones++;
+    }
+
+    public void Retirar(float cantidad) {
+        
+        saldo -= cantidad;
+        numeroRetiros++;
+    }
+
+    public void ExtraerMensual() {
+       
+        saldo -= comisionMensual;
+    }
+
+    public void Imprimir() {
+      
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Número de consignaciones: " + numeroConsignaciones);
+        System.out.println("Número de retiros: " + numeroRetiros);
+        System.out.println("Tasa anual: " + tasaAnual);
+        System.out.println("Comisión mensual: " + comisionMensual);
     }
 }
+  
